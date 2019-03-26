15c9e43658b01e21eab62151f8fdaf64134835b3
AShariff0786 <45340721+AShariff0786@users.noreply.github.com>
2019-03-26 12:54:27 -0400

    Update Robbery.java

--- Robbery.java
+++ Robbery.java
@@ -5,24 +5,45 @@
 // You are encouraged to make helper functions!
 
 public class Robbery {
+   
+   //Finds the max value
+   public int max(int x, int y) {
+      if(x>=y) {
+         return x;
+      }
+      else
+         return y;
+   }
 
 	// Using DP: Get the maximum value with capacity C and n items
-	public int maximizeRobWorthRecur(
-		int capacity,
-		int[] sizes,
-		int[] worths
-	) {
+	public int maximizeRobWorthRecur(int capacity, int[] sizes, int[] worths, int length) {
 		// fill in here, change the return
-			return 0;
+	   if(length==1 && sizes[0]<=capacity) {
+	      return worths[0];
+	   }
+	   int weight=sizes[sizes.length-1];
+	   if(weight<=capacity) {
+	      return worths[sizes.length-1] + maximizeRobWorthRecur((capacity-weight), sizes, worths, length-1); 
+	   }
+	   else
+	      return maximizeRobWorthRecur(capacity, sizes, worths, length-1);
 	}
 
-	public int maximizeRobWorthBottomUp(
-		int capacity,
-		int[] sizes,
-		int[] worths
-	) {
+	public int maximizeRobWorthBottomUp(int capacity, int[] sizes, int[] worths) {
 		// fill in here, change the return
-		return 0;
+	   int max=0;
+      int [][] memoarr=new int[sizes.length+1][capacity+1];
+      for(int i=1; i<=sizes.length; i++) {
+        for(int j=1; j<=capacity; j++) {
+          if(sizes[i-1]<= j) {
+            memoarr[i][j]= max(memoarr[i-1][j], memoarr[i-1][j-(sizes[i-1])] + worths[i-1]);
+           }
+           else
+               memoarr[i][j]= memoarr[i-1][j];
+        }
+      }
+      max= memoarr[sizes.length][capacity];
+		return max;
 	}
 
 /**
@@ -41,7 +62,7 @@ public class Robbery {
 		int[] itemSizes = {2, 25, 6, 13, 1, 15, 8, 5, 17, 4};
 		int[] itemWorths = {35, 120, 900, 344, 29, 64, 67, 95, 33, 10};
 
-		int maxWorthRecur = r.maximizeRobWorthRecur(bagCapacity, itemSizes, itemWorths);
+		int maxWorthRecur = r.maximizeRobWorthRecur(bagCapacity, itemSizes, itemWorths, itemSizes.length);
 		System.out.println("Max worth of the bag: " + maxWorthRecur);
 		int maxWorthBottomUp = r.maximizeRobWorthBottomUp(bagCapacity, itemSizes, itemWorths);
 		System.out.println("Max worth of the bag: " + maxWorthBottomUp);
